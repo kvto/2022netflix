38553c4a89be7c63c14678f256b2e9c2dace5c39
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-08-30 07:40:46 -0500

    kev - 2022netflix

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { styled} from '@mui/material/styles';
 
 function App() {
+  const classes = useStyles();
+  const user = null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={classes.root}>
+       <Routes>
+        {
+          !user ? (<Login />) : (
+            <Route path="/profile" element={<Profile />} />,
+            <Route path="/checkout" element={<Paypal />} />,
+            <Route path="/" element={<Home />} />
+          )
+        }
+      </Routes>
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) =>({
+  root: {
+    backgroundColor: "#111",
+    minHeight: "100vh"
+  }
+}))
 
 export default App;
