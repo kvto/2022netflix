5e61a188513677996861b5fcb451cc1863501bd6
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-09-28 17:04:28 -0500

    kev - 2022netflix

--- src/Pages/Paypal.js
+++ src/Pages/Paypal.js
@@ -5,53 +5,42 @@ import {selectPrice} from "../features/counter/priceSlice"
   
 const Paypal = () => {
 
-  <script>
-  <script>
-    function initPayPalButton() {
-    const paypal = useRef();
-      paypal.Buttons({
-        style: {
-          shape: 'rect',
-          color: 'gold',
-          layout: 'vertical',
-          label: 'paypal',
+      // paypal.Buttons({
+      //   style: {
+      //     shape: 'rect',
+      //     color: 'gold',
+      //     layout: 'vertical',
+      //     label: 'paypal',
           
-        },
+      //   },
 
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [{"description":"Pagar","amount":{"currency_code":"USD","value":12}}]
-          });
-        },
+      //   createOrder: function(data, actions) {
+      //     return actions.order.create({
+      //       purchase_units: [{"description":"Pagar","amount":{"currency_code":"USD","value":12}}]
+      //     });
+      //   },
 
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(orderData) {
+      //   onApprove: function(data, actions) {
+      //     return actions.order.capture().then(function(orderData) {
             
-            // Full available details
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+      //       // Full available details
+      //       console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
 
-            // Show a success message within this page, e.g.
-            const element = document.getElementById('paypal-button-container');
-            element.innerHTML = '';
-            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+      //       // Show a success message within this page, e.g.
+      //       const element = document.getElementById('paypal-button-container');
+      //       element.innerHTML = '';
+      //       element.innerHTML = '<h3>Thank you for your payment!</h3>';
 
-            // Or go to another URL:  actions.redirect('thank_you.html');
+      //       // Or go to another URL:  actions.redirect('thank_you.html');
             
-          });
-        },
+      //     });
+      //   },
 
-        onError: function(err) {
-          console.log(err);
-        }
-      }).render('#paypal-button-container')
+      //   onError: function(err) {
+      //     console.log(err);
+      //   }
+      // }).render('#paypal-button-container')
     }
-    initPayPalButton();
-  </script>
-
-  </script>
- 
-  
-}
 
 
 export default Paypal
